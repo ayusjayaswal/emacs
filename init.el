4c592aa5e3562b553ff436d4c6d36992bad5e3e0
ayusjayaswal <ayushjayaswal@yandex.com>
2023-07-06 14:32:21 +0530

    Few Changes

--- init.el
+++ init.el
@@ -144,11 +144,11 @@
 
 (use-package which-key
   :init
-  (which-key-mode 1))
-  ;;:config
-   ;;(setq which-key-sort-uppercase-first nil
-         ;;which-key-idle-delay 0.8
-         ;;which-key-separator " → " ))
+  (which-key-mode 1)
+  :config
+   (setq which-key-sort-uppercase-first nil
+         which-key-idle-delay 0.8
+         which-key-separator " → " ))
 
 ;  (setq ring-bell-function 'ignore)
 
